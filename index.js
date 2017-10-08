6d16c342a8a86c608c0e20bcc1cd3c768ede893c
stopvoldemort <david.squires1@gmail.com>
2017-10-08 17:51:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+  customerName = (customerName.toUpperCase())
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "davey"
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "mike"
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "davey"
+  let favoriteCustomer = "mikey"
+}
